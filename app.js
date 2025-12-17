ced39baee321cebd129d8bd4456ffbf7ca2a6ebe
Steny005 <stenytr05@gmail.com>
2025-12-17 14:46:49 +0530

    websitee

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattl
 const options = {
 	method: 'GET',
 	headers: {
+		'x-rapidapi-key': '7f0858be17msh9edb5ad6a749723p1c9428jsn629dedf5d649',
 		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+fetch(url, options),
+then(response => response.json())
+.then(response => console.log(response))
+.catch(err =>  console.error(err));	
+	
