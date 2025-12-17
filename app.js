de5aacc054ca86830c4ca0c477f14b621a6a31ed
Steny005 <stenytr05@gmail.com>
2025-12-17 14:18:16 +0530

    websitee

---
+++ app.js
@@ -0,0 +1,15 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
